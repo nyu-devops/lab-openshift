0747a520e797dfcec1cad98425a4421e09e1518e
John Rofrano <johnnyroy@johnrofrano.com>
2024-04-11 19:27:05 -0400

    Update setup-lab.sh
    Added the following:
    
    ```
    git config --global --add safe.directory /app
    ```
    
    To make git stop complain about unsafe folders

--- .devcontainer/scripts/setup-lab.sh
+++ .devcontainer/scripts/setup-lab.sh
@@ -2,6 +2,11 @@
 echo "Setting up Docker lab environment..."
 docker pull quay.io/rofrano/python:3.11-slim
 # docker run -d --name postgresql --restart always -p 5432:5432 -v postgres:/var/lib/postgresql/data postgres:alpine
+
 echo Setting up registry.local...
 sudo bash -c "echo '127.0.0.1    cluster-registry' >> /etc/hosts"
+
+echo "Making git stop complaining about unsafe folders"
+git config --global --add safe.directory /app
+
 echo "Setup complete"
