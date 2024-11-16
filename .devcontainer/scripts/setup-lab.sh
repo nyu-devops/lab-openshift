e1cb938fb405a5c61275e7a328eefffda639a0e9
John Rofrano <johnnyroy@johnrofrano.com>
2024-11-16 07:30:38 -0500

    Updated tools to latest levels

--- .devcontainer/scripts/setup-lab.sh
+++ .devcontainer/scripts/setup-lab.sh
@@ -7,7 +7,7 @@ echo "Pulling custom Python:3.11-slim image from quay.io..."
 docker pull quay.io/rofrano/python:3.11-slim
 # docker run -d --name postgresql --restart always -p 5432:5432 -v postgres:/var/lib/postgresql/data postgres:alpine
 
-echo Setting up registry.local...
+echo "Setting up registry.local..."
 sudo bash -c "echo '127.0.0.1    cluster-registry' >> /etc/hosts"
 
 echo "Making git stop complaining about unsafe folders"
